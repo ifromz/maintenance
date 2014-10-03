0df7b5d595433c135d1c7aa540fe03f128cd2de0
Steve Bauman <steven_bauman_7@hotmail.com>
2014-10-03 14:00:34 -0400

    Lot's of tweaks and updates. Restored folders back to standard position, modified controller namespacing. Modified all services create and update methods to allow easier create and update for optional data entry.

--- src/models/WorkOrderSupply.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Stevebauman\Maintenance\Models;
-
-use Stevebauman\Maintenance\Models\BaseModel;
-
-class WorkOrderSupply extends BaseModel {
-	
-	protected $table = 'work_order_supplies';
-	
-}
