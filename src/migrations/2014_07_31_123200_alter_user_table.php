1759b27eba534e8d52082206d969d1a0b41ae2de
Steve Bauman <steven_bauman_7@hotmail.com>
2015-01-20 13:36:54 -0500

    Fixed alter user table migration

--- src/migrations/2014_07_31_123200_alter_user_table.php
+++ src/migrations/2014_07_31_123200_alter_user_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class AlterUserTable extends Migration
@@ -13,12 +14,16 @@ class AlterUserTable extends Migration
     public function up()
     {
         if (Schema::hasTable('users')) {
+
             if (!Schema::hasColumn('users', 'username')) {
-                Schema::table('users', function (Closure $table) {
+
+                Schema::table('users', function (Blueprint $table) {
                     $table->string('username')->after('email');
                     $table->unique('username');
                 });
+
             }
+
         }
     }
 
