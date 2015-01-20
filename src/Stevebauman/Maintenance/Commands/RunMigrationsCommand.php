a1733d40e1e2654a88ecf8447c9cbeb8e2c34127
Steve Bauman <steven_bauman_7@hotmail.com>
2015-01-20 13:26:21 -0500

    Updated with vendor migration in the install command instead of bench

--- src/Stevebauman/Maintenance/Commands/RunMigrationsCommand.php
+++ src/Stevebauman/Maintenance/Commands/RunMigrationsCommand.php
@@ -22,7 +22,7 @@ class RunMigrationsCommand extends Command {
 
     public function fire()
     {
-        $this->call('migrate', array('--env' => $this->option('env'), '--bench' => 'stevebauman/maintenance' ) );
+        $this->call('migrate', array('--env' => $this->option('env'), '--vendor' => 'stevebauman/maintenance' ) );
     }
 
 }
