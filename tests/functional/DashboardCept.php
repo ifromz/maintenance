739d75e54e241ccdeac99414a940b9013d243264
Steve Bauman <steven_bauman_7@hotmail.com>
2015-01-21 14:16:09 -0500

    Updated tests

--- tests/functional/DashboardCept.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$I = new FunctionalTester($scenario);
-$user = Sentry::findUserById(1);
-Sentry::login($user, false);
-$I->wantTo('Make sure dashboard works');
-$I->amOnRoute('maintenance.dashboard.index');
-$I->see('Dashboard');
