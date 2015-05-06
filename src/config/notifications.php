7133352c04c61e6c78699f51329ad82b9a400b59
Steve Bauman <steven_bauman@outlook.com>
2015-05-06 12:05:58 -0400

    Added default icon

--- src/config/notifications.php
+++ src/config/notifications.php
@@ -4,12 +4,15 @@ return [
 
     /**
      * The maintenance application notification icons.
-     * A different icon will be displayed depending on the model that created it
+     *
+     * A different icon will be displayed depending on the model that created it.
      */
     'icons' => [
         'Stevebauman\Maintenance\Models\WorkOrder'                  => 'fa fa-book success',
         'Stevebauman\Maintenance\Models\WorkOrderAssignment'        => 'fa fa-flag danger',
         'Stevebauman\Maintenance\Models\InventoryStock'             => 'fa fa-dropbox info',
+
+        'default' => 'fa fa-bell-o info',
     ],
 
 ];
