73f06671e243cd8ab220481f6eb8fa51238e90ae
Steve Bauman <steven_bauman@outlook.com>
2015-05-06 12:06:55 -0400

    Added more command exceptions

---
+++ src/Stevebauman/Maintenance/Exceptions/Commands/ComponentNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Stevebauman\Maintenance\Exceptions\Commands;
+
+use Stevebauman\Maintenance\Exceptions\BaseException;
+
+class ComponentNotFoundException extends BaseException
+{
+}
---
+++ src/Stevebauman/Maintenance/Exceptions/Commands/ModelNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Stevebauman\Maintenance\Exceptions\Commands;
+
+use Stevebauman\Maintenance\Exceptions\BaseException;
+
+class ModelNotFoundException extends BaseException
+{
+}
