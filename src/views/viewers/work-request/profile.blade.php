76c47d13eb60004d0e9b44d16ce4323c49956fa5
Steve Bauman <steven_bauman@outlook.com>
2015-05-22 10:41:43 -0400

    Tweaked work request profile

--- src/views/viewers/work-request/profile.blade.php
+++ src/views/viewers/work-request/profile.blade.php
@@ -1,23 +1,20 @@
-<dl class="dl-horizontal">
-
-    <dt>Created By:</dt>
-    <dd>{{ $workRequest->user->full_name }}</dd>
-
-    <p></p>
-
-    <dt>Best Time:</dt>
-    <dd>{{ $workRequest->best_time }}</dd>
-
-    <p></p>
-
-    <dt>Subject:</dt>
-    <dd>{{ $workRequest->subject }}</dd>
-
-    <p></p>
-
-    <dt>Description:</dt>
-    <dd>{{ $workRequest->description }}</dd>
-
-    <p></p>
-
-</dl>
+<table class="table">
+    <tbody>
+        <tr>
+            <th>Created By</th>
+            <td>{{ $workRequest->user->full_name }}</td>
+        </tr>
+        <tr>
+            <th>Best Time</th>
+            <td>{{ $workRequest->best_time }}</td>
+        </tr>
+        <tr>
+            <th>Subject</th>
+            <td>{{ $workRequest->suject }}</td>
+        </tr>
+        <tr>
+            <th>Description</th>
+            <td>{{ $workRequest->description }}</td>
+        </tr>
+    </tbody>
+</table>
