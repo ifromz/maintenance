8d38b6a7d1fad97bc48eff08f10e8babde37e028
Steve Bauman <steven_bauman@outlook.com>
2015-05-25 12:22:51 -0400

    Added the currentUser variable available to all views

--- src/Stevebauman/Maintenance/Composers/MainLayoutComposer.php
+++ src/Stevebauman/Maintenance/Composers/MainLayoutComposer.php
@@ -2,6 +2,7 @@
 
 namespace Stevebauman\Maintenance\Composers;
 
+use Stevebauman\Maintenance\Services\SentryService;
 use Stevebauman\Maintenance\Services\ConfigService;
 use Illuminate\View\View;
 
@@ -11,25 +12,38 @@ use Illuminate\View\View;
 class MainLayoutComposer
 {
     /**
+     * @var SentryService
+     */
+    protected $sentry;
+
+    /**
      * @var ConfigService
      */
     protected $config;
 
     /**
+     * Constructor.
+     *
+     * @param SentryService $sentry
      * @param ConfigService $config
      */
-    public function __construct(ConfigService $config)
+    public function __construct(SentryService $sentry, ConfigService $config)
     {
+        $this->sentry = $sentry;
         $this->config = $config->setPrefix('maintenance');
     }
 
     /**
+     * Applies site wide variables to main layout.
+     *
      * @param $view
      */
     public function compose(View $view)
     {
         $siteTitle = $this->config->get('site.title.main');
+        $currentUser = $this->sentry->getCurrentUser();
 
         $view->with('siteTitle', $siteTitle);
+        $view->with('currentUser', $currentUser);
     }
 }
