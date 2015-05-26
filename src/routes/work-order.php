c47b27251a88cfe369e4dc660a0b2c0520f1d5e3
Steve Bauman <steven_bauman@outlook.com>
2015-05-26 12:45:48 -0400

    Added work order user session view

--- src/routes/work-order.php
+++ src/routes/work-order.php
@@ -117,6 +117,11 @@ Route::group(['namespace' => 'WorkOrder'], function () {
     |--------------------------------------------------------------------------
     */
 
+    Route::get('work-orders/{work_orders}/sessions/user/{users}', [
+        'as' => 'maintenance.work-orders.sessions.show-user',
+        'uses' => 'SessionController@showUser'
+    ]);
+
     Route::post('work-orders/{work_orders}/sessions/start', [
         'as' => 'maintenance.work-orders.session.start',
         'uses' => 'SessionController@postStart',
