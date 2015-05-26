afb42a900b046bdb0309201799df101196c5dbfe
Steve Bauman <steven_bauman@outlook.com>
2015-05-26 13:00:30 -0400

    Added base viewer code docs

--- src/Stevebauman/Maintenance/Viewers/BaseViewer.php
+++ src/Stevebauman/Maintenance/Viewers/BaseViewer.php
@@ -6,8 +6,10 @@ use Stevebauman\Viewer\AbstractViewer;
 
 class BaseViewer extends AbstractViewer
 {
-    /*
-     * Allows all child viewers to display their records history
+    /**
+     * Returns the records history view.
+     *
+     * @return \Illuminate\View\View
      */
     public function history()
     {
