d848fdf3adfad8b65b551b395f014eae8a3f63a8
Steve Bauman <steven_bauman@outlook.com>
2015-05-26 12:59:56 -0400

    Added attendee viewer code docs

--- src/Stevebauman/Maintenance/Viewers/AttendeeViewer.php
+++ src/Stevebauman/Maintenance/Viewers/AttendeeViewer.php
@@ -4,6 +4,11 @@ namespace Stevebauman\Maintenance\Viewers;
 
 class AttendeeViewer extends BaseViewer
 {
+    /**
+     * Returns the attendees status view.
+     *
+     * @return \Illuminate\View\View
+     */
     public function status()
     {
         return view('maintenance::viewers.event.attendee.status', [
@@ -11,6 +16,11 @@ class AttendeeViewer extends BaseViewer
         ]);
     }
 
+    /**
+     * Returns the attendees action button view.
+     *
+     * @return \Illuminate\View\View
+     */
     public function btnActions()
     {
         return view('maintenance::viewers.event.attendee.buttons.actions', [
