77ff959687d1f9b254dbac72646af909157dab3e
Steve Bauman <steven_bauman@outlook.com>
2015-06-01 16:41:24 -0400

    Tweaked alert partial

--- src/views/layouts/partials/alert.blade.php
+++ src/views/layouts/partials/alert.blade.php
@@ -1,29 +1,28 @@
 <div class="row">
-
-    @if ($errors->any())
-        <div class="col-md-12">
-            <div class="notifications alert alert-danger alert-block">
-                <button type="button" class="close" data-dismiss="alert"><i class="fa fa-minus-square"></i></button>
-                @if ($message = $errors->first(0, ':message'))
-                    {{ $message }}
-                @else
-                    There were errors with the form you've sent.
-                @endif
-            </div>
-        </div>
-    @endif
-
-    @if ($message = Session::get('success'))
-        <div class="col-md-12">
-            <div class="notifications alert alert-success alert-block">
-                <button type="button" class="close" data-dismiss="alert"><i class="fa fa-minus-square"></i></button>
-                {{ $message }}
-            </div>
-        </div>
-    @endif
-
     <div class="col-md-12">
         <div id="alert-container">
+            @if ($errors->any())
+                <div class="col-md-12">
+                    <div class="notifications alert alert-danger alert-block">
+                        <button type="button" class="close" data-dismiss="alert"><i class="fa fa-minus-square"></i></button>
+                        @if ($message = $errors->first(0, ':message'))
+                            {{ $message }}
+                        @else
+                            There were errors with the form you've sent.
+                        @endif
+                    </div>
+                </div>
+            @endif
+
+            @if ($message = Session::get('success'))
+                <div class="col-md-12">
+                    <div class="notifications alert alert-success alert-block">
+                        <button type="button" class="close" data-dismiss="alert"><i class="fa fa-minus-square"></i></button>
+                        {{ $message }}
+                    </div>
+                </div>
+            @endif
+
             @if(Session::get('message'))
                 <div class="col-md-12">
                     <div class="alert alert-{{ Session::get('messageType') }} alert-dismissable">
@@ -34,5 +33,4 @@
             @endif
         </div>
     </div>
-
 </div>
