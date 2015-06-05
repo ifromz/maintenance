b2a8b6e9e46ee5d956159fe503d9be1ae7fdfea7
Steve Bauman <steven_bauman@outlook.com>
2015-06-05 17:03:21 -0400

    Reworking part stock controller

--- src/Stevebauman/Maintenance/Http/Controllers/WorkOrder/Part/StockController.php
+++ src/Stevebauman/Maintenance/Http/Controllers/WorkOrder/Part/StockController.php
@@ -2,70 +2,34 @@
 
 namespace Stevebauman\Maintenance\Http\Controllers\WorkOrder\Part;
 
-use Stevebauman\Maintenance\Validators\WorkOrder\PartPutBackValidator;
-use Stevebauman\Maintenance\Validators\WorkOrder\PartTakeValidator;
-use Stevebauman\Maintenance\Services\Inventory\StockMovementService;
-use Stevebauman\Maintenance\Services\Inventory\StockService;
-use Stevebauman\Maintenance\Services\Inventory\InventoryService;
-use Stevebauman\Maintenance\Services\WorkOrder\WorkOrderService;
+use Stevebauman\Maintenance\Http\Requests\WorkOrder\Part\ReturnRequest;
+use Stevebauman\Maintenance\Http\Requests\WorkOrder\Part\TakeRequest;
+use Stevebauman\Maintenance\Repositories\Inventory\Repository as InventoryRepository;
+use Stevebauman\Maintenance\Repositories\WorkOrder\Repository as WorkOrderRepository;
 use Stevebauman\Maintenance\Http\Controllers\Controller;
 
 class StockController extends Controller
 {
     /**
-     * @var WorkOrderService
+     * @var WorkOrderRepository
      */
     protected $workOrder;
 
     /**
-     * @var InventoryService
+     * @var InventoryRepository
      */
     protected $inventory;
 
     /**
-     * @var StockService
-     */
-    protected $inventoryStock;
-
-    /**
-     * @var StockMovementService
-     */
-    protected $inventoryStockMovement;
-
-    /**
-     * @var PartTakeValidator
-     */
-    protected $workOrderPartTakeValidator;
-
-    /**
-     * @var PartPutBackValidator
-     */
-    protected $workOrderPartPutBackValidator;
-
-    /**
      * Constructor.
      *
-     * @param WorkOrderService $workOrder
-     * @param InventoryService $inventory
-     * @param StockService $inventoryStock
-     * @param StockMovementService $inventoryStockMovement
-     * @param PartTakeValidator $workOrderPartTakeValidator
-     * @param PartPutBackValidator $workOrderPartPutBackValidator
+     * @param WorkOrderRepository $workOrder
+     * @param InventoryRepository $inventory
      */
-    public function __construct(
-        WorkOrderService $workOrder,
-        InventoryService $inventory,
-        StockService $inventoryStock,
-        StockMovementService $inventoryStockMovement,
-        PartTakeValidator $workOrderPartTakeValidator,
-        PartPutBackValidator $workOrderPartPutBackValidator
-    ) {
+    public function __construct(WorkOrderRepository $workOrder, InventoryRepository $inventory)
+    {
         $this->workOrder = $workOrder;
         $this->inventory = $inventory;
-        $this->inventoryStock = $inventoryStock;
-        $this->inventoryStockMovement = $inventoryStockMovement;
-        $this->workOrderPartTakeValidator = $workOrderPartTakeValidator;
-        $this->workOrderPartPutBackValidator = $workOrderPartPutBackValidator;
     }
 
     /**
@@ -104,9 +68,13 @@ class StockController extends Controller
         return view('maintenance::work-orders.parts.inventory.stocks.take', compact('workOrder', 'item', 'stock'));
     }
 
-    public function postTake($workOrderId, $inventoryId, $stockId)
+    public function postTake(TakeRequest $request, $workOrderId, $inventoryId, $stockId)
     {
+        if($this->workOrder->takePart($request, $workOrderId, $stockId)) {
 
+        } else {
+
+        }
     }
 
     public function getPut($workOrderId, $inventoryId, $stockId)
@@ -115,202 +83,12 @@ class StockController extends Controller
         $item = $this->inventory->find($inventoryId);
     }
 
-    public function postPut($workOrderId, $inventoryId, $stockId)
+    public function postPut(ReturnRequest $request, $workOrderId, $inventoryId, $stockId)
     {
-        $workOrder = $this->workOrder->find($workOrderId);
-        $item = $this->inventory->find($inventoryId);
-    }
+        if($this->workOrder->returnPart($request, $workOrderId, $stockId)) {
 
-    /**
-     * Display the form to update the quantity the
-     * user is taking from the inventory for the work order.
-     *
-     * @param int|string $workOrder_id
-     * @param int|string $inventory_id
-     * @param int|string $stock_id
-     *
-     * @return \Illuminate\View\View
-     */
-    public function create($workOrder_id, $inventory_id, $stock_id)
-    {
-        $workOrder = $this->workOrder->find($workOrder_id);
-        $item = $this->inventory->find($inventory_id);
-        $stock = $this->inventoryStock->find($stock_id);
-
-        return view('maintenance::work-orders.parts.stocks.create', [
-            'title' => 'Enter Quantity Used',
-            'workOrder' => $workOrder,
-            'item' => $item,
-            'stock' => $stock,
-        ]);
-    }
-
-    /**
-     * Process the quantity the user is taking from the stock location.
-     *
-     * @param int|string $workOrder_id
-     * @param int|string $inventory_id
-     * @param int|string $stock_id
-     *
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
-     */
-    public function store($workOrder_id, $inventory_id, $stock_id)
-    {
-        if ($this->workOrderPartTakeValidator->passes()) {
-            $workOrder = $this->workOrder->find($workOrder_id);
-            $item = $this->inventory->find($inventory_id);
-            $stock = $this->inventoryStock->find($stock_id);
-
-            /*
-             * Grab all input data
-             */
-            $data = $this->inputAll();
-
-            /*
-             * Add Part to work order (passing in the work order and the stock)
-             */
-            $this->workOrder->setInput($data)->savePart($workOrder, $stock);
-
-            /*
-             * Set the extra input data for the inventory stock change reason
-             */
-            $data['reason'] = sprintf('Used for <a href="%s">Work Order</a>', route('maintenance.work-orders.show', [$workOrder->id]));
-
-            /*
-             * Perform a take from the stock
-             */
-            $this->inventoryStock->setInput($data)->take($stock->id);
-
-            /*
-             * Set the return messages
-             */
-            $this->message = sprintf(
-                'Successfully added %s of %s to work order. %s or %s',
-                $this->input('quantity'),
-                $item->name,
-                link_to_route('maintenance.work-orders.parts.index', 'Add More', [$workOrder->id]),
-                link_to_route('maintenance.work-orders.show', 'View Work Order', [$workOrder->id])
-            );
-
-            $this->messageType = 'success';
-            $this->redirect = route('maintenance.work-orders.parts.index', [$workOrder->id]);
         } else {
-            $this->errors = $this->workOrderPartTakeValidator->getErrors();
-            $this->redirect = route('maintenance.work-orders.parts.stocks.create', [
-                $workOrder_id, $inventory_id, $stock_id,
-            ]);
+
         }
-
-        return $this->response();
-    }
-
-    /**
-     * Destroys the pivot table entry of the stock quantity used in the work order,
-     * then returns the quantity back to the stock and creates a movement indicating
-     * that the quantity of the item was put back from a work order.
-     *
-     * @param int|string $workOrder_id
-     * @param int|string $inventory_id
-     * @param int|string $stock_id
-     *
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
-     */
-    public function postPutBack($workOrder_id, $inventory_id, $stock_id)
-    {
-        $workOrder = $this->workOrder->find($workOrder_id);
-        $item = $this->inventory->find($inventory_id);
-        $stock = $this->inventoryStock->find($stock_id);
-
-        /*
-         * Find the specific work order part record from the stock id
-         */
-        $record = $workOrder->parts->find($stock->id);
-
-        /*
-         * Set the reason and quantity of why the putting back is taking place
-         */
-        $data = [
-            'reason' => sprintf('Put back from <a href="%s">Work Order</a>', route('maintenance.work-orders.show', [$workOrder->id])),
-            'quantity' => $record->pivot->quantity,
-        ];
-
-        /*
-         * Update the inventory stock record
-         */
-        $this->inventoryStock->setInput($data)->put($stock->id);
-
-        /*
-         * Remove the part from the work order
-         */
-        $workOrder->parts()->detach($stock->id);
-
-        $this->message = sprintf('Successfully put back %s into the inventory', $item->name);
-        $this->messageType = 'success';
-        $this->redirect = route('maintenance.work-orders.show', [$workOrder->id]);
-
-        return $this->response();
-    }
-
-    /**
-     * Processes a put back some operation on the specified
-     * inventory stock from the specified work order.
-     *
-     * @param int|string $workOrder_id
-     * @param int|string $inventory_id
-     * @param int|string $stock_id
-     *
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
-     */
-    public function postPutBackSome($workOrder_id, $inventory_id, $stock_id)
-    {
-        $workOrder = $this->workOrder->find($workOrder_id);
-        $item = $this->inventory->find($inventory_id);
-        $stock = $this->inventoryStock->find($stock_id);
-
-        /*
-         * Find the specific work order part record from the stock id
-         */
-        $part = $workOrder->parts->find($stock->id);
-
-        /*
-         * Add less than rule so users must enter a number below the quantity that
-         * was taken for the work order
-         */
-        $this->workOrderPartPutBackValidator->addRule('quantity', 'less_than:'.$part->pivot->quantity);
-
-        if ($this->workOrderPartPutBackValidator->passes()) {
-
-            /*
-             * Set the reason and quantity of why the putting back is taking place
-             */
-            $data = [
-                'reason' => sprintf('Put back from <a href="%s">Work Order</a>', route('maintenance.work-orders.show', [$workOrder->id])),
-                'quantity' => $this->input('quantity'),
-            ];
-
-            /*
-             * Update the inventory stock record
-             */
-            $this->inventoryStock->setInput($data)->put($stock->id);
-
-            /*
-             * Set the new pivot quantity
-             */
-            $newQuantity = $part->pivot->quantity - $this->input('quantity');
-
-            /*
-             * Update the existing pivot record
-             */
-            $workOrder->parts()->updateExistingPivot($stock->id, ['quantity' => $newQuantity]);
-
-            $this->message = sprintf('Successfully put back %s into the inventory', $item->name);
-            $this->messageType = 'success';
-            $this->redirect = route('maintenance.work-orders.show', [$workOrder->id]);
-        } else {
-            $this->errors = $this->workOrderPartPutBackValidator->getErrors();
-            $this->redirect = route('maintenance.work-orders.parts.stocks.index', [$workOrder_id, $inventory_id]);
-        }
-
-        return $this->response();
     }
 }
