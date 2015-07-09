bd0d10daa1567f76da7c1b52aab48548ac6bb0c1
Steve Bauman <steven_bauman@outlook.com>
2015-07-09 13:17:12 -0400

    Default colspan of 10 to make sure it covers all columns

--- src/Views/assets/images/grid/templates/no-results.blade.php
+++ src/Views/assets/images/grid/templates/no-results.blade.php
@@ -1,7 +1,7 @@
 <script type="text/template" data-grid="assets-images" data-template="no_results">
 
     <tr>
-        <td colspan="7" class="text-center">There are no images to display.</td>
+        <td colspan="10" class="text-center">There are no images to display.</td>
     </tr>
 
 </script>
