fd80ec7d49400b026ae3963c696e066f8f845900
Steve Bauman <steven_bauman@outlook.com>
2015-07-12 15:16:30 -0400

    Make calendar full-screen

--- src/Views/dashboard/index.blade.php
+++ src/Views/dashboard/index.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="box box-primary">
                 <div class="box-body no-padding">
 
