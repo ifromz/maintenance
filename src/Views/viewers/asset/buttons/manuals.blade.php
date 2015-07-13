dccc73c50770092fff41ea8939bb1855cbc36815
Steve Bauman <steven_bauman@outlook.com>
2015-07-13 16:39:46 -0400

    Fix button route

--- src/Views/viewers/asset/buttons/manuals.blade.php
+++ src/Views/viewers/asset/buttons/manuals.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ route('maintenance.assets.manuals.create', [$asset->id]) }}" class="btn btn-app no-print">
+<a href="{{ route('maintenance.assets.manuals.index', [$asset->id]) }}" class="btn btn-app no-print">
     <i class="fa fa-file-text"></i> Manuals ({{ $asset->manuals->count() }})
 </a>
